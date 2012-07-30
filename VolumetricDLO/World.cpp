97584819f78cb833315ff2f74c144d101535a7d7
Teodor Cioaca <t.cioaca@gmail.com>
2012-07-30 09:26:22 +0300

    FIX: torsion forces instead of penalty displacements

--- VolumetricDLO/World.cpp
+++ VolumetricDLO/World.cpp
@@ -36,7 +36,7 @@ void World::Display()
 	glColor3f(1,0,0);
 
 	World::GetInstance()->dlo->Render();
-	World::GetInstance()->dlo->PerformUpdateStep();
+	//World::GetInstance()->dlo->PerformUpdateStep();
 
 	glutSwapBuffers();
 }
